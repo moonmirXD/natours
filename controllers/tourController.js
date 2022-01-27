89bdf3dfd93b7686ff1c11c46aad0074f6df062b
Ryan Arqueza <arquezaryan@gmail.com>
2022-01-27 14:50:21 +0800

    first commit

---
+++ controllers/tourController.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkID = (req, res, next, value) => {
+  if (value * 1 > tours.length) {
+    return res.status(404).json({
+      staus: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  const inputs = Object.keys(req.body);
+  const allowedProperties = ['name', 'price'];
+  const isOperationValid = allowedProperties.every((input) =>
+    inputs.includes(input)
+  );
+
+  if (!isOperationValid) {
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'Missing name and price' });
+  }
+
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated data>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
