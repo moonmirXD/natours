8217108ed04b865c2d2ac3291970d29f4b380fb6
Ryan Arqueza <arquezaryan@gmail.com>
2022-02-02 15:08:13 +0800

    Handling unhandled routes

--- app.js
+++ app.js
@@ -12,13 +12,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   console.log("I'm middleware");
-//   next();
-// });
-
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.json({
+    status: ' fail',
+    message: `Can't find ${req.originalUrl} route`,
+  });
+});
 
 module.exports = app;
