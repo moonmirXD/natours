336bb82f131cfdfe60afabbd99ef7c246ac01c99
Ryan Arqueza <arquezaryan@gmail.com>
2022-02-04 17:53:24 +0800

    Managing password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a confirm password'],
     minlenth: 8,
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'The passwords must match!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model('User', userSchema);
