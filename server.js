396f705bd2b73c8cf8bb9e53d3d90dd37ca67cae
Ryan Arqueza <arquezaryan@gmail.com>
2022-02-04 11:27:08 +0800

    Catching uncaught exception

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION ERROR! 🔥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
