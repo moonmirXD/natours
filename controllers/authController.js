0f53ffda3e7c83e4828fc2557b34b635fd878d38
Ryan Arqueza <arquezaryan@gmail.com>
2022-02-07 20:32:44 +0800

    Protecting tour routes part 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,3 +47,22 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+
+  console.log(req.headers);
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in, please log in!', 401));
+  }
+
+  next();
+});
