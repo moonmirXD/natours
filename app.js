1d69e3cb42cb0827d1a0cc539ebab48319fb4439
Ryan Arqueza <arquezaryan@gmail.com>
2022-02-19 17:54:28 +0800

    Preventing parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +34,18 @@ app.use(express.static(`${__dirname}/public`));
 app.use(mongoSanitize());
 app.use(xss());
 
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
