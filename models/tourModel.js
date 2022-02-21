48481c88a342eb5beaa4417ab6a32b130007c205
Ryan Arqueza <arquezaryan@gmail.com>
2022-02-21 14:35:14 +0800

    Modelling tour guides embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -101,6 +102,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -117,10 +119,12 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-// tourSchema.post('save', (document, next) => {
-//   console.log('saved');
-//   next();
-// });
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  console.log(this.guides);
+  next();
+});
 
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
